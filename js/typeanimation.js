e0759a448c767af0a9f3d91f9df12bd0e1dfdfce
Darren Smith <contact@darrensmith.com.au>
2020-10-16 10:35:44 +1100

    changes

---
+++ js/typeanimation.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('.typed', {
+         strings: ["Monitor", "Benchmark", "Optimise"],
+         typeSpeed: 80,
+         loop: true,
+         backSpeed: 40,
+         backDelay: 3000
+         });
